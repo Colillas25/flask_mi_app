20f440c776fdbad3edbb029bf2c503adf6ebbef6
David <david.noguera7@gmail.com>
2025-08-07 18:45:57 +0200

    aaaaaaaaaaaa

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Muestra la pagina de la aplicacion
+    
+    devuelve:
+        string: archivo html de index.html
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
